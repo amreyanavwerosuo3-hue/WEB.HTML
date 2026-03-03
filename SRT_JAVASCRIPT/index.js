28e0dc1d82425d279f27f230824fb0f0393796ea
justice amreyan <amreyanavwerosuo3@gmail.com>
2026-03-03 11:05:03 +0000

    completing javascript

--- SRT_JAVASCRIPT/index.js
+++ SRT_JAVASCRIPT/index.js
@@ -1,1 +1,22 @@
  console.log('hello alex how are you');
+// variables
+
+let age =30;
+let name= 'james';
+
+console.log(`${name} is ${age} years old thi year`);
+
+
+// seatwork
+
+let NAME ='choima';
+let parent_name ='emmanuels';
+
+console.log(`${NAME} is the first child of are parents which is the family of ${parent_name} `)
+
+const testscore = 70;
+const examscore = 100;
+
+const total = testscore+examscore;
+
+console.log(`his testscore ${testscore} and his examscore is ${examscore} so the total he scored is ${total}`)
